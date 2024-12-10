03a3b21089e1e0825b53e6d2fa8815e8cf35746e
kaiem1 <abdurrahmankaiem03@gmail.com>
2024-12-11 02:50:52 +0600

    use params, navigate

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,19 +1,28 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 
 const Post = ({post}) => {
     const {id, title} = post;
+
+    const navigate = useNavigate();
+
     const postStyle = {
         border: '2px solid yellow',
         padding: '10px',
         borderRadius: '20px',
-       
     }
+
+    const handleShowDetail = () =>{
+        navigate(`/post/${id}`);
+    }
+
     return (
         <div style={postStyle}>
             <h2>Post of Id: {id}</h2>
             <p>{title}</p>
             <Link to={`/post/${id}`}>Post Detail</Link>
+            <Link to={`/post/${id}`}> <button>Show Details</button></Link>
+            <button onClick={handleShowDetail}>Click to see details</button>
         </div>
     );
 };
